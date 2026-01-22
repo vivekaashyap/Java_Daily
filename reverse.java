3328724a469ad000ba87aa2587ad5fe1fcd1d267
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-01-22 20:23:58 +0530

    update

--- reverse.java
+++ reverse.java
@@ -1,17 +1,20 @@
-public class reverse{
-    public static void main(String[] args){
+
+public class reverse {
+
+    public static void main(String[] args) {
         String s = "           Hello           World        ";
         String t = s.trim();
         String[] arr = t.split(" ");
         String res = "";
-        for(int i = arr.length-1; i>=0; i--){
+        for (int i = arr.length - 1; i >= 0; i--) {
             String word = arr[i];
-            if(word.length()!=0){
-                res+= word;
-                if(i!=0){
-                    res+=" ";
+            if (word.length() != 0) {
+                res += word;
+                if (i != 0) {
+                    res += " ";
                 }
             }
-        }System.out.println(res);
+        }
+        System.out.println(res);
     }
 }
