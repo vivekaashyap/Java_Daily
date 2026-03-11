1d1fd75fc329cd76f02867e477d5789d3a5700a6
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-03-11 22:08:41 +0530

    push new code

--- Arrays/_16MergeSort.java
+++ Arrays/_16MergeSort.java
@@ -6,4 +6,12 @@ public class _16MergeSort {
         int arr2[] = {6, 8, 7, 9, 10};
 
     }
+
+    public static void mergeSort(int arr1[], int arr2[]) {
+        int len = arr1.length + arr2.length;
+        int[] finalarr = new int[len];
+        for (int i = 0; i < arr1.length; i++) {
+            finalarr[i] =  
+        }
+    }
 }
