1cca037096d089d563c32853a127a63cedd86bea
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-07-08 22:07:21 +0530

    hehe

---
+++ Divide & Conquer/MergeSort.java
@@ -0,0 +1,15 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int arr[] = { 6, 7, 3, 2, 1, 8, 9 };
+
+    }
+    public dsafkj
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
